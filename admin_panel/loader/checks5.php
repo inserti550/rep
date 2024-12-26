73410296580e7966ae2b348db854cc5a23f694eb
inserti550 <166512415+inserti550@users.noreply.github.com>
2024-12-26 02:26:45 +0200

    Update checks5.php

--- admin_panel/loader/checks5.php
+++ admin_panel/loader/checks5.php
@@ -1,9 +1,3 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo "003/zzzzzzzzzzzzzzzzzzzz/128563200";
-    exit;
-} else {
-    http_response_code(405);
-    echo "Method Not Allowed";
-}
+echo "003/zzzzzzzzzzzzzzzzzzzz/128563200"; exit;
 ?>
